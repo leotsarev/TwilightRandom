476c2ba864af31dea2cc9614bee970395abf6397
Leonid Tsarev <leo@leotsarev.ru>
2022-09-10 23:00:54 +0300

    Add project files.

---
+++ TwilightRandom/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TwilightRandom
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var randomizer = new Randomiser();
+            var result = randomizer.Randomize();
+            foreach(var x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("Невыбранные фракции");
+            foreach (var f in randomizer.Factions)
+            {
+                Console.WriteLine(f);
+
+            }
+        }
+    }
+}
---
+++ TwilightRandom/Randomiser.cs
@@ -0,0 +1,107 @@
+﻿using MoreLinq;
+
+namespace TwilightRandom
+{
+    internal class Randomiser
+    {
+        public HashSet<string> Factions = new(new[]
+{
+            "The Arborec",
+            "The Barony of Letnev",
+            "The Clan of Saar",
+            "The Embers of Muaat (Тлеющие с Муаата)",
+            "The Emirates of Hacan",
+            "The Federation of Sol",
+            "The Ghosts of Creuss (Призраки Креусса)",
+            "The L1Z1X Mindnet",
+            "The Mentak Coalition",
+            "The Naalu Collective",
+            "The Nekro Virus (Некровирус)",
+            "Sardakk N’orr",
+            "The Universities of Jol-Nar (Университеты Жол-Нара)",
+            "The Winnu",
+            "The Xxcha Kingdom",
+            "The Yin Brotherhood",
+            "The Yssaril Tribes",
+            "The Argent Flight",
+            "The Empyrean",
+            "The Mahact Gene-Sorcerers",
+            "The Naaz-Rokha Alliance",
+            "The Nomad (Кочевник)",
+            "The Titans of Ul (Титаны Ула)",
+            "The Vuil'Raith Cabal (Кабал вуил’райт)",
+        });
+
+        public HashSet<string> Colors = new(new[]
+        {
+            "Черный",
+            "Желтый",
+            "Фиолетовый",
+            "Зеленый",
+            "Синий",
+            "Оранжевый",
+            "Красный",
+            "Розовый",
+        });
+
+        public HashSet<string> Players = new(new[]
+        {
+            "@leotsarev",
+            "@GrayFox23b",
+            "@Germesina",
+            //"@Warpo",
+            "@Korran_Horn",
+            "@blakotya",
+            "@Werrus",
+            "Руслан",
+        });
+
+        public class PlayerRandomizeResult
+        {
+            public string PlayerName { get; set; }
+            public string Color { get; set; }
+            public string[] Factions { get; set; }
+
+            public override string ToString()
+            {
+                return $"{PlayerName} - {Color} - {string.Join(",", Factions)}";
+            }
+        }
+
+        public PlayerRandomizeResult[] Randomize()
+        {
+            while (Players.Count < 8)
+            {
+                Players.Add($"Запасной игрок {Players.Count + 1}");
+            }
+
+            var playersDict = Players.ToDictionary(player => player, player => new PlayerRandomizeResult { PlayerName = player });
+
+            if (playersDict.TryGetValue("@Germesina", out var germesina))
+            {
+                germesina.Color = "Черный";
+                Colors.Remove(germesina.Color);
+            }
+
+            foreach (var result in playersDict.Values.Where(r => r.Color is null))
+            {
+                result.Color = SelectAndRemoveRandom(Colors);
+            }
+
+            foreach (var result in playersDict.Values.Where(r => r.Factions is null))
+            {
+                result.Factions = new[] { SelectAndRemoveRandom(Factions), SelectAndRemoveRandom(Factions) };
+            }
+
+            return playersDict.Values.Shuffle().ToArray();
+        }
+
+        private static string SelectAndRemoveRandom(HashSet<string> set)
+        {
+            var idx = Random.Shared.Next(0, set.Count);
+            var selected = set.ElementAt(idx);
+            set.Remove(selected);
+            return selected;
+        }
+    }
+}
