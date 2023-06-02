2882670c252a1fec68b982f05ea3a3cafe417bdf
Leonid Tsarev <leo@leotsarev.ru>
2023-06-02 16:46:39 +0300

    Update everything and merge

---
+++ Twilight.Web/Migrations/20230602134139_FixData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Twilight.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+    table: "Factions",
+    keyColumn: "Id",
+    keyValue: 8,
+    column: "WikiLink",
+    value: "https://twilight-imperium.fandom.com/wiki/The_L1Z1X_Mindnet");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
