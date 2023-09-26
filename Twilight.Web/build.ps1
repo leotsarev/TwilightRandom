6c6325d4468e940037b8795adbff542a0a501a5d
Leonid Tsarev <leo@leotsarev.ru>
2023-09-26 15:35:39 +0300

    Move tagging and and pushing directly to build

--- Twilight.Web/build.ps1
+++ Twilight.Web/build.ps1
@@ -2,7 +2,5 @@
     [Parameter(Mandatory=$true)][string]$version
  )
 dotnet publish --os linux --arch x64 -c Release /p:PublishProfile=DefaultContainer /p:Version=$version
-docker tag twilight-web:$version cr.yandex/crp3fr717nr1rn78qeij/twilight-web:$version
-docker push cr.yandex/crp3fr717nr1rn78qeij/twilight-web:$version
 #ฯฮฬลอ฿าÜ ย DEPLOYMENT.YML
 #kubectl apply -f .\deployment.yml -n twilight
