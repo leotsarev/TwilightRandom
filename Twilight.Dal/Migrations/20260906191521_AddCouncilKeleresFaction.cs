95bbe2b4d714cbc53c09a428f7258731a153b174
leotsarev <leo@leotsarev.ru>
2026-09-06 22:17:13 +0300

    Add The Council Keleres faction

---
+++ Twilight.Dal/Migrations/20260906191521_AddCouncilKeleresFaction.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Twilight.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCouncilKeleresFaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Factions",
+                columns: new[] { "Id", "Name", "RussianName", "WikiLink" },
+                values: new object[] { 30, "The Council Keleres", "Совет Келерес", "https://twilight-imperium.fandom.com/wiki/The_Council_Keleres" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Factions",
+                keyColumn: "Id",
+                keyValue: 30);
+        }
+    }
+}
